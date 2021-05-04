2958a0597810f1aa18816c1af4f24eb4e6e9dd68
Devilseye04 <vineetkv9703@gmail.com>
2021-05-04 22:29:39 +0530

    footer

--- client/src/components/PostComponent.vue
+++ client/src/components/PostComponent.vue
@@ -58,11 +58,11 @@
 
         <div id="footer">
           <p class="lf">
-            Copyright &copy; 2010 <a href="#">SiteName</a> - All Rights Reserved
+            Copyright &copy; 2021 
           </p>
           <p class="rf">
             Design by
-            <a href="http://chocotemplates.com/">ChocoTemplates.com</a>
+            <a href="https://github.com/Devilseye04">VineetKV</a>
           </p>
 
           <div style="clear: both"></div>
